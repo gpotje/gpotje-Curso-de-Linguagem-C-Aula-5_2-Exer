2c128d2e2a81504fd86482cdb95bcd9c4209a91e
gabriel potje <gpotje.souza@gmail.com>
2020-08-19 11:01:02 -0300

    5_2 aula e exer

---
+++ pratico5_2 -alinhado.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+#include <locale.h>
+
+main(){
+		
+	setlocale(LC_ALL,"Portuguese");
+	
+	int N1, N2 ;
+			
+  	printf("Digite o primeiro numero: ");
+  	scanf("%i", &N1);
+  			
+  	printf("Digite o segundo numero: ");
+	scanf("%i", &N2);
+
+ 	if (N1 == N2)
+      printf("Os numeros são iguais!");
+    	else{
+    		if(N1 > N2)
+				printf("O maior valor é = %i", N1);
+					else
+						printf("O maior valor é = %i", N2);}
+					 
+					 
+}
